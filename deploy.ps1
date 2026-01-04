0bbf8ae4022f26b829972e065916c280584c5e01
mchrrouhi <mchrrouhi@gmail.com>
2026-01-04 03:15:58 +0100

    ok

--- deploy.ps1
+++ deploy.ps1
@@ -1,20 +1,14 @@
 # Deployment rapide vers Hostinger
 Write-Host "Deploiement en cours..." -ForegroundColor Cyan
 
-# Build si necessaire
-if (-not (Test-Path "dist")) {
-    Write-Host "Build du projet..." -ForegroundColor Yellow
-    npm run build
-}
-
-# Upload tous les fichiers
+# Upload tous les fichiers et dossiers
 Write-Host "Upload vers Hostinger..." -ForegroundColor Yellow
-cd dist
-scp -i "..\deploy_key" -P 65002 -r .htaccess * u118534042@185.201.10.97:public_html/
-cd ..
+scp -i "deploy_key" -P 65002 -r .htaccess index.html blog.html blog-post.html style.css script.js assets/ oil/ tshirt/ test1/ test2/ savona/ product2/ public/ u118534042@185.201.10.97:public_html/
 
 Write-Host ""
 Write-Host "DEPLOIEMENT TERMINE!" -ForegroundColor Green
 Write-Host "Site: https://emavory.com" -ForegroundColor Cyan
+Write-Host "Oil: https://emavory.com/oil" -ForegroundColor Cyan
+Write-Host "T-Shirt: https://emavory.com/tshirt" -ForegroundColor Cyan
 Write-Host "Test1: https://emavory.com/test1" -ForegroundColor Cyan
 Write-Host "Test2: https://emavory.com/test2" -ForegroundColor Cyan
